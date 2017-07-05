9b952363502672b6cd4305844411e87334ff2cd4
Anders Miltner <Anders.Miltner@gmail.com>
2017-07-05 15:07:09 -0400

    random changes
    removed pcf from prog names, removed use of boomerang when bijective lenses sufficient
    
    removed extraction specs to let git accept

--- combine-data.py
+++ combine-data.py
@@ -9,12 +9,6 @@ from os.path import splitext, join
 import subprocess
 import sys
 import time
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-
-matplotlib.rcParams['mathtext.fontset'] = 'stix'
-matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 TEST_EXT = '.ls'
 BASELINE_EXT = '.out'
@@ -138,7 +132,7 @@ def print_data(data):
 	datawriter.writerows(data)
 
 def print_usage(args):
-    print("Usage: {0} <file1>".format(args[0]))
+    print("Usage: {0} <file1> <file2> <file3>".format(args[0]))
 
 def transform_data(path, base, run_data):
     current_data = {"Test":join(path, base + TEST_EXT).replace("_","-")[6:]}
@@ -161,159 +155,31 @@ def retrieve_csv(filename):
             csv_rows.append(row)
     return csv_rows
 
-def project_column_from_csv(csv_obj, col_name):
-    return [r[col_name] for r in csv_obj]
-
-def write_to_filename(filename, s):
-    with open(filename, "wb") as f:
-        f.write(s)
-
-def generate_examples_required_data(input_csv):
-    zero_count_ind = 0
-    one_to_five_count_ind = 1
-    six_to_ten_count_ind = 2
-    eleven_to_fifteen_count_ind = 3
-    sixteen_to_twenty_count_ind = 4
-    over_twenty_count_ind = 5
-
-    zero_count_text = "0"
-    one_to_five_count_text = "1-5"
-    six_to_ten_count_text = "6-10"
-    eleven_to_fifteen_count_text = "11-15"
-    sixteen_to_twenty_count_text = "16-20"
-    over_twenty_count_text = ">20"
-    ind_to_text = [zero_count_text,
-                   one_to_five_count_text,
-                   six_to_ten_count_text,
-                   eleven_to_fifteen_count_text,
-                   sixteen_to_twenty_count_text,
-                   over_twenty_count_text]
-
-    experimental_values = [0,0,0,0,0,0,]
-    determinizing_values = [0,0,0,0,0,0,]
-
-    def add_to_correct_group(count_values, n):
-        if n < 0.0:
-            raise Exception("SOMETHING WENT WRONG")
-        if n == 0.0:
-            count_values[zero_count_ind] = count_values[zero_count_ind]+1
-        elif n <= 5.0:
-            count_values[one_to_five_count_ind] = count_values[one_to_five_count_ind]+1
-        elif n <= 10.0:
-            count_values[six_to_ten_count_ind] = count_values[six_to_ten_count_ind]+1
-        elif n <= 15.0:
-            count_values[eleven_to_fifteen_count_ind] = count_values[eleven_to_fifteen_count_ind]+1
-        elif n <= 20.0:
-            count_values[sixteen_to_twenty_count_ind] = count_values[sixteen_to_twenty_count_ind]+1
-        else:
-            count_values[over_twenty_count_ind] = count_values[over_twenty_count_ind]+1
-
-    experimental_vals = project_column_from_csv(input_csv, "ExamplesRequired")
-    for example_num in experimental_vals:
-        add_to_correct_group(experimental_values, float(example_num))
-    determinizing_vals = project_column_from_csv(input_csv, "MaxExampleCount")
-    for example_num in determinizing_vals:
-        add_to_correct_group(determinizing_values, float(example_num))
-    
-
-    ind = np.arange(6)
-    width = 0.35
-
-    fig, ax = plt.subplots()
-
-    rects1 = ax.bar(ind, experimental_values, width, color='#ffffb3', align='center')
-    rects2 = ax.bar(ind+width, determinizing_values, width, color='#998ec3', align='center')
-
-    ax.set_ylabel('Count')
-    ax.set_title("Examples Required for Benchmarks")
-    ax.set_xticks(ind + width / 2)
-    ax.set_xticklabels(ind_to_text)
-
-    ax.legend((rects1[0],rects2[0]),("Experimental Average", "Determinize Permutations"))
-
-    plt.savefig("examples.eps")
-
-def generate_uninferred_expansions_data(input_csv):
-    zero_count_ind = 0
-    one_to_five_count_ind = 1
-    six_to_ten_count_ind = 2
-    eleven_to_fifteen_count_ind = 3
-    sixteen_to_twenty_count_ind = 4
-    over_twenty_count_ind = 5
-
-    zero_count_text = "0"
-    one_count_text = "1"
-    two_count_text = "2"
-    three_count_text = "3"
-    four_count_text = "4"
-    ind_to_text = [zero_count_text,
-                   one_count_text,
-                   two_count_text,
-                   three_count_text,
-                   four_count_text]
-
-    uninferred_values = [0,0,0,0,0,]
-    unforced_values = [0,0,0,0,0,]
-
-    def add_to_correct_group(count_values, n):
-        if n == 0.0:
-            count_values[zero_count_ind] = count_values[zero_count_ind]+1
-        elif n == 1.0:
-            count_values[one_to_five_count_ind] = count_values[one_to_five_count_ind]+1
-        elif n == 2.0:
-            count_values[six_to_ten_count_ind] = count_values[six_to_ten_count_ind]+1
-        elif n == 3.0:
-            count_values[eleven_to_fifteen_count_ind] = count_values[eleven_to_fifteen_count_ind]+1
-        elif n == 4.0:
-            count_values[sixteen_to_twenty_count_ind] = count_values[sixteen_to_twenty_count_ind]+1
-        else:
-            raise Exception("SOMETHING WENT WRONG")
-
-    total_exps = project_column_from_csv(input_csv, "ExpansionsPerformedNoLensContext")
-    forced_exps = project_column_from_csv(input_csv, "ExpansionsForcedNoLensContext")
-    inferred_exps = project_column_from_csv(input_csv, "ExpansionsInferredNoLensContext")
-    total_and_inferred = zip(total_exps, inferred_exps)
-    total_and_forced = zip(total_exps, forced_exps)
-    for (total_exp,forced_exp) in total_and_forced:
-        add_to_correct_group(unforced_values, float(total_exp)-float(forced_exp))
-    for (total_exp,inferred_exp) in total_and_inferred:
-        add_to_correct_group(uninferred_values, float(total_exp)-float(inferred_exp))
-
-    ind = np.arange(5)
-    width = 0.35
-
-    fig, ax = plt.subplots()
-
-    rects1 = ax.bar(ind, uninferred_values, width, color='#ffffb3', align='center')
-    rects2 = ax.bar(ind+width, unforced_values, width, color='#998ec3', align='center')
-
-    ax.set_ylabel('Count')
-    ax.set_title("Examples Required for Benchmarks")
-    ax.set_xticks(ind + width / 2)
-    ax.set_xticklabels(ind_to_text)
-
-    ax.legend((rects1[0],rects2[0]),("Full Inference","Forced Only"))
-
-    plt.savefig("uninferred.eps")
-
-def generate_time_vs_tasks_data(input_csv):
-    def create_step_plot(colname, outputname):
-        col_vals = project_column_from_csv(input_csv, colname)
-        col_vals_and_endpoints = [x for x in col_vals if x != "-1"] + [0,600000]
-        x_vals = sorted([float(x) for x in set(col_vals_and_endpoints)])
-
-        print(x_vals)
-        return 0
-    create_step_plot("FlashExtract","FlashExtract")
-
 def main(args):
-    if len(args) == 2:
-        input_filepath = args[1]
-        input_csv = retrieve_csv(input_filepath)
-        ensure_dir("generated-graphs/")
-        generate_examples_required_data(input_csv)
-        generate_uninferred_expansions_data(input_csv)
-        generate_time_vs_tasks_data(input_csv)
+    if len(args) == 4:
+        prog1_path = args[1]
+        prog2_path = args[2]
+        prog3_path = args[3]
+        ensure_dir("paper/generated-data/")
+        csv1 = retrieve_csv(prog1_path)
+        csv2 = retrieve_csv(prog2_path)
+        csv3 = retrieve_csv(prog3_path)
+        data_keyed_by_test = {r["Test"]: r for r in csv1}
+        for row in csv2:
+            relevant_data = data_keyed_by_test[row["Test"]]
+            for key, value in row.items():
+                relevant_data[key] = value
+        for row in csv3:
+            relevant_data = data_keyed_by_test[row["Test"]]
+            for key, value in row.items():
+                relevant_data[key] = value
+        print(data_keyed_by_test)
+        with open("paper/generated-data/data.csv", "wb") as csvfile:
+            fieldnames = data_keyed_by_test[data_keyed_by_test.keys()[0]].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(list(data_keyed_by_test.values()))
     else:
         print_usage(args)
 
---
+++ program/holder/swap.ls
@@ -0,0 +1,4 @@
+typedef A = "a";;
+typedef B = "b";;
+
+x = ["1" A "2" B "3" <=> "1" B "2" A "3" {}]
